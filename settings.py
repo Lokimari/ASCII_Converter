ae415fc66dbb0d3212a6ef3f8a60b678c7e6e40b
Noah <nhbb920@gmail.com>
2022-07-16 02:55:46 -0500

    add settings.py

---
+++ settings.py
@@ -0,0 +1,5 @@
+# Main vars / settings - Change values as you wish
+LIGHT_LEVELS =    [char for char in ".,':;^*!([%#"]
+INVERSE_SHADING = False
+PADDING =         1 * " "    # Padding between pixels
+DIAGONAL_OFFSET = 0  # Positive offsets down+right, negative down+left
